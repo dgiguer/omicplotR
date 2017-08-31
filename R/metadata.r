c56876f7042709455bd79010ab10913226533c90
dgiguer <dgiguer@uwo.ca>
2017-08-31 11:43:07 -0400

    added example data

---
+++ R/metadata.r
@@ -0,0 +1,17 @@
+#' Vaginal microbiome OTU table metadata
+#'
+#' Associated metadata to \code{\link{otu_table}}. 
+#'
+#' @name metadata
+#'
+#' @docType data
+#'
+#' @references Macklaim et al (2014). Microb Ecol Health Dis.
+#' doi: http://dx.doi.org/10.3402/mehd.v26.27799
+#'
+#' @format A data frame with 297 rows and 35 columns, where rows are samples
+#'  and columns are collected metadata.
+#'
+#' @seealso \code{\link{otu_table}}
+#'
+NULL
---
+++ R/otu_table.r
@@ -0,0 +1,18 @@
+#' Vaginal microbiome OTU table
+#'
+#' This dataset contains a count table of operational taxonomic units (OTUs)
+#'  from a 16s rRNA gene sequencing experiment characterising changes in
+#'  bacterial microbiota from oral antimicrobial or probiotic treatments.
+#'  Associated metadata is contained in \code{\link{metadata}}.
+#'
+#' @name otu_table
+#'
+#' @docType data
+#'
+#' @references Macklaim et al (2014). Microb Ecol Health Dis.
+#' doi: http://dx.doi.org/10.3402/mehd.v26.27799
+#'
+#' @format A data frame with 77 rows and 298 columns. The last column contains
+#'  taxonomic information for each OTU. Samples are columns, OTUs are rows.
+#'
+NULL
